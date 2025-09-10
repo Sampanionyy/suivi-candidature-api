76a6f0740d5701487536e28b25e3c108b1330c6e
Sampaniony Rakotoarivelo <sampanionyra55@gmail.com>
2025-09-10 21:00:57 +0200

    feat: reminder scheduler ok

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('interviews:send-reminders')
+    ->dailyAt('09:00') // Heure fixe
+    ->withoutOverlapping()
+    ->appendOutputTo(storage_path('logs/interview-reminders.log'))
+    ->description('📧 Rappels d\'entretien (TEST)');
